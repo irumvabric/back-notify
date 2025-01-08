63e31d6baad21fc4996178f70f4020edfcc95695
irumvabric <irumvabric#@gmail.com>
2025-01-08 13:40:48 +0200

    Added Static root

--- notify_vac/settings.py
+++ notify_vac/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = "/root/biu/Brice/back-notify/static/"
+
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
