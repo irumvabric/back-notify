75dcd746fb3b75459f119a7c56ada3d0386491c4
irumvabric <irumvabric#@gmail.com>
2025-01-13 12:46:37 +0200

    Following the proton guy

--- notify_vac/settings.py
+++ notify_vac/settings.py
@@ -71,6 +71,9 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+"whitenoise.middleware.WhiteNoiseMiddleware",
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -152,7 +155,13 @@ USE_TZ = True
 STATIC_ROOT = "/root/biu/Brice/back-notify/static/"
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
